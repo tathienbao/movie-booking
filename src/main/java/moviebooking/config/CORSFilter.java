f6d84303bc288888b54a018c99fca8d145b88130
tathienbao <tathienbao.ttb@gmail.com>
2025-11-04 01:31:40 +0200

    fix: Handle preflight OPTIONS requests in CORS filter

--- src/main/java/moviebooking/config/CORSFilter.java
+++ src/main/java/moviebooking/config/CORSFilter.java
@@ -1,14 +1,24 @@
 package moviebooking.config;
 
 import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
 
 import java.io.IOException;
 
 @Provider
-public class CORSFilter implements ContainerResponseFilter {
+public class CORSFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        // Handle preflight requests
+        if (requestContext.getMethod().equalsIgnoreCase("OPTIONS")) {
+            requestContext.abortWith(Response.ok().build());
+        }
+    }
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
